70e85913fcbdfda28a12ec157ff78d13fd7b411f
Jonathan <jonathanbgjo@gmail.com>
2020-12-12 18:32:41 -0800

    like functtions

--- app/controllers/api/likes_controller.rb
+++ app/controllers/api/likes_controller.rb
@@ -1,2 +1,16 @@
 class Api::LikesController < ApplicationController
+    def create
+
+    end
+
+    def destroy
+
+    end
+
+    private
+
+    def likes_param
+        params.require(:like).permit(:video_id, :user_id)
+
+    end
 end
