2cb50b304af1f6b2832e03737df548b409fa4e34
Jonathan <jonathanbgjo@gmail.com>
2020-12-16 15:58:32 -0800

    initialize likes react component

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_22_223748) do
+ActiveRecord::Schema.define(version: 2020_12_08_223926) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,11 @@ ActiveRecord::Schema.define(version: 2020_10_22_223748) do
     t.string "body", null: false
     t.integer "author_id", null: false
     t.integer "video_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "likes", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
---
+++ frontend/components/video_show/likes/likes.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+
+class Likes extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            // body: ""
+        }
+        this.handleSubmit = this.handleSubmit.bind(this)
+        this.handleChange = this.handleChange.bind(this)
+        console.log(this.props)
+    }
+    handleChange(e) {
+        this.setState({
+            body: e.currentTarget.value
+        })
+    }
+    handleSubmit(e) {
+        e.preventDefault();
+        this.props.createComment(comment)
+        this.setState({
+            // body: ""
+        })
+        // this.props.fetchVideo(this.props.video.id)
+    }
+    componentDidMount() {
+        // this.props.fetchComments(this.props.video.id)
+    }
+    // componentDidUpdate(prevProps){
+    //     if (prevProps.match.params.videoId !== this.props.match.params.videoId) {
+    //         this.props.fetchComments(this.props.video.id)
+    //     }
+    // }
+    render() {
+        // let comments = Object.values(this.props.comments).map((comment) => {
+        //     return <CommentIndexItem key={comment.id} comment={comment} />
+        // })
+
+        return (
+            <div className="likesContainer">
+            </div>
+        )
+    }
+}
+
+export default CommentIndex;
