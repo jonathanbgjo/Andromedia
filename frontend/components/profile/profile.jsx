7dc9d76519395940a94642f62318ececad18eba7
Jonathan <jonathanbgjo@gmail.com>
2021-03-08 23:21:37 -0800

    profile descriptions

--- frontend/components/profile/profile.jsx
+++ frontend/components/profile/profile.jsx
@@ -11,7 +11,13 @@ class Profile extends React.Component {
     }
     render(){
         return(
-            <div> Am i in {this.props.currentUser.username}'s' profile!</div>
+            <>
+
+                <div> Am i in {this.props.currentUser.username}'s' profile!</div>
+                <span> profile pic -- username -- # subscribers</span>
+                <span> do we want multiple tabs?</span>
+                <span> Videos you've uploaded | Videos you've liked or commented on</span>
+            </>
         )
     }
 }
